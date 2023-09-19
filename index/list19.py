3f75b373d45ba6a0c8b1203aa81ce770c51ff642
swethathangapandiyan <swethat.sightspectrum@gmail.com>
2023-09-19 05:51:54 -0700

    first commit

---
+++ index/list19.py
@@ -0,0 +1,53 @@
+a=["apple","cherry",2,4,"banana",5,7,8,9]
+print(a)
+print(type(a))
+print(a[0:2])
+print(a[3])
+print(a[-5])
+print(a[1:5])
+a.insert(2,"berry")
+print(a)
+a.pop()
+print(a)
+a.remove(2)
+print(a)
+print(a[2:7])
+print(a[:5])
+print(a[::2])
+print(a[-6:-4])
+print(a[-4:-6])
+b="swetha"
+print(b[::3])
+print(b[0:5:2])
+list=[1,2,3,5,["a","b","c",],91]
+print(list[4][1:])
+b=["python","c","java","c++"]
+b.sort()
+print(b)
+a.append("string")
+print(a)
+var={"N":{"a","b","c",(1,2,3,4,(2,4,6,8))},"age":[21,22,23,24]}
+N={"a","b","c",(1,2,3,4,(2,4,6,8))}
+print(N)
+print(var.keys())
+print(var["N"])
+for x in var["N"]:
+    #print(x)
+    for y in x:
+        print(y)
+a.extend(["something"])
+print(a)
+a.insert(3,"banana")
+print(a)
+a.remove(5)
+print(a)
+print(a.pop())
+b=a.index("cherry")
+print(b)
+a.reverse()
+print(a)
+
+
+
+
+
